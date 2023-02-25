eb8f864aed0b779c527a28f3265d7b6d88729445
asifalam515 <asibulalam515@gmail.com>
2023-02-25 15:07:41 +0600

    state lifted

--- src/components/tasks/CreateTasks.jsx
+++ src/components/tasks/CreateTasks.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 
 const CreateTasks = (props) => {
-    const [text,setText]=useState('sample task')
+    const [text,setText]=useState('')
     return (
         <div>
             <input type="text" placeholder='Type Your Task' value={text} onChange={(event)=>{
                 setText(event.target.value)
+                
             }} />
             <button onClick={()=>{
-                props.addNewTask(text)
+                if(text){
+                    props.addNewTask(text)
+                setText('')
+                }
+                else{
+                    alert('No text added')
+                }
             }}>Create Task</button>
             
         </div>
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 import CreateTasks from '../components/tasks/CreateTasks';
+import ShowTasks from '../components/tasks/ShowTasks';
 
 const Tasks = () => {
-const [tasks,setTaks]=useState([])
+const [tasks,setTasks]=useState([])
 const [visibility,setVisibility]=useState('all')
 const addNewTask=(text)=>{
-    console.log('text calling from main component')
-    console.log(text)
-  
+    const task={
+        text,
+        isCompleted:false,
+        cretedAt:new Date(),
+    }
+setTasks([task,...tasks])
 }
     return (
         <div>
             <h1>Your Tasks</h1>
             <CreateTasks addNewTask={addNewTask}></CreateTasks>
+            <ShowTasks tasks={tasks}></ShowTasks>
         </div>
     );
 };
